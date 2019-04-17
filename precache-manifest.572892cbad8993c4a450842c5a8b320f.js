a93a999443871d5b2a05622b700aab5f41c73c5c
Adilkhan <khan17171717@gmail.com>
2019-04-17 13:56:46 +0600

    first

---
+++ precache-manifest.572892cbad8993c4a450842c5a8b320f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d0943a499c2f9fe345",
+    "url": "/static/css/main.f1ef470f.chunk.css"
+  },
+  {
+    "revision": "a3d0943a499c2f9fe345",
+    "url": "/static/js/main.483acbc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "c4cea77e7fd2af688474e5e38a944040",
+    "url": "/index.html"
+  }
+];
